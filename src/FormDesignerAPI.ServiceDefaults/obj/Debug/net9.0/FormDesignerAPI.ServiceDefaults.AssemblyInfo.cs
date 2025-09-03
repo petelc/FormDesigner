19e53695c69836913a273aabbea1707f93d6e01a
Peter Carroll <petelc1966@hotmail.com>
2025-09-03 18:48:46 -0400

    cache files

--- src/FormDesignerAPI.ServiceDefaults/obj/Debug/net9.0/FormDesignerAPI.ServiceDefaults.AssemblyInfo.cs
+++ src/FormDesignerAPI.ServiceDefaults/obj/Debug/net9.0/FormDesignerAPI.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormDesignerAPI.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9a739e851a7fe082efc76d1d267449a3969036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bdfbee5cd7b9ab82602fae1ac562809e4b8d237")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormDesignerAPI.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormDesignerAPI.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
