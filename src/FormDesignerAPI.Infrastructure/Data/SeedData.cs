a42e078aabbdaeb9c7bfec1d4d7c162574b43b00
Peter Carroll <petelc1966@hotmail.com>
2025-09-08 04:13:36 -0400

    Working on Seed Data

--- src/FormDesignerAPI.Infrastructure/Data/SeedData.cs
+++ src/FormDesignerAPI.Infrastructure/Data/SeedData.cs
@@ -1,22 +1,35 @@
 ﻿using FormDesignerAPI.Core.ContributorAggregate;
+using FormDesignerAPI.Core.FormAggregate;
 
 namespace FormDesignerAPI.Infrastructure.Data;
 
 public static class SeedData
 {
-  public static readonly Contributor Contributor1 = new("Ardalis");
-  public static readonly Contributor Contributor2 = new("Snowfrog");
+  public static readonly Contributor Contributor1 = new Contributor("Ardalis");
+  public static readonly Contributor Contributor2 = new Contributor("Snowfrog");
+
+  public static readonly Form Form1 = new Form("FD-001", "Form Designer Initial Form")
+    .UpdateDivision("Engineering")
+    .UpdateVersion("1.0")
+    .SetOwner("John Doe", "john.doe@example.com");
+
+  public static readonly Form Form2 = new Form("FD-002", "Form Designer Second Form")
+    .UpdateDivision("Marketing")
+    .UpdateVersion("1.0")
+    .SetOwner("Jane Smith", "jane.smith@example.com");
 
   public static async Task InitializeAsync(AppDbContext dbContext)
   {
     if (await dbContext.Contributors.AnyAsync()) return; // DB has been seeded
+    if (await dbContext.Forms.AnyAsync()) return; // DB has been seeded
 
     await PopulateTestDataAsync(dbContext);
   }
 
   public static async Task PopulateTestDataAsync(AppDbContext dbContext)
   {
-    dbContext.Contributors.AddRange([Contributor1, Contributor2]);
+    dbContext.Contributors.AddRange(new[] { Contributor1, Contributor2 });
+    dbContext.Forms.AddRange(new[] { Form1, Form2 });
     await dbContext.SaveChangesAsync();
   }
 }
