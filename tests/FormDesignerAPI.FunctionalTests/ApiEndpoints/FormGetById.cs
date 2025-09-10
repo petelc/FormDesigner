86c367fd5d2da523192eda5a5c051a01e4c63d4d
Peter Carroll <petelc1966@hotmail.com>
2025-09-10 18:11:53 -0400

    finished existing functional tests

--- tests/FormDesignerAPI.FunctionalTests/ApiEndpoints/FormGetById.cs
+++ tests/FormDesignerAPI.FunctionalTests/ApiEndpoints/FormGetById.cs
@@ -1,8 +1,25 @@
-using System;
+using FormDesignerAPI.Infrastructure.Data;
+using FormDesignerAPI.Web.Forms;
 
 namespace FormDesignerAPI.FunctionalTests.ApiEndpoints;
 
-public class FormGetById
+public class FormGetById(CustomWebApplicationFactory<Program> factory) : IClassFixture<CustomWebApplicationFactory<Program>>
 {
+    private readonly HttpClient _client = factory.CreateClient();
 
+    [Fact]
+    public async Task ReturnsSeedFormGivenId1()
+    {
+        var result = await _client.GetAndDeserializeAsync<FormRecord>(GetFormByIdRequest.BuildRoute(1));
+
+        result.Id.ShouldBe(1);
+        result.FormNumber.ShouldBe(SeedData.Form1.FormNumber);
+    }
+
+    [Fact]
+    public async Task ReturnsNotFoundGivenId1000()
+    {
+        string route = GetFormByIdRequest.BuildRoute(1000);
+        _ = await _client.GetAndEnsureNotFoundAsync(route);
+    }
 }
