a4bb01a4a669c2c44094d01f3843b9bee4e2b7cc
Peter Carroll <petelc1966@hotmail.com>
2025-09-27 05:19:12 -0400

    Subject line (try to keep under 50 characters) cache update Multi-line description of commit, feel free to be detailed.
    [Ticket: X]

--- src/FormDesignerAPI.Web/obj/Debug/net9.0/FormDesignerAPI.Web.AssemblyInfo.cs
+++ src/FormDesignerAPI.Web/obj/Debug/net9.0/FormDesignerAPI.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormDesignerAPI.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efebeed8d1a489599d85010c85e4702984b31f9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea545b483c0e5653124438dc5d9d87905a5badef")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormDesignerAPI.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormDesignerAPI.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
