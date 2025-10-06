f8146519c2cf36e0bc763a94945d4375f0f30c24
Peter Carroll <petelc1966@hotmail.com>
2025-10-06 06:02:47 -0400

    cache

--- src/FormDesignerAPI.Web/obj/Debug/net9.0/FormDesignerAPI.Web.AssemblyInfo.cs
+++ src/FormDesignerAPI.Web/obj/Debug/net9.0/FormDesignerAPI.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormDesignerAPI.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf6f07a51877c1f2867cead2e215a969568c802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1491050ff4992151e9db5e4a8afc41230b13ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormDesignerAPI.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormDesignerAPI.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
