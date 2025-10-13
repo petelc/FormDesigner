4ea8af53c1d37225db792ccb55e64d08c6259446
Peter Carroll <petelc1966@hotmail.com>
2025-10-13 06:00:01 -0400

    working on get user roles

---
+++ src/FormDesignerAPI.UseCases/Identity/Role/GetUserRoles/GetUserRolesHandler.cs
@@ -0,0 +1,25 @@
+using FormDesignerAPI.UseCases.Interfaces;
+
+namespace FormDesignerAPI.UseCases.Identity.Role.GetUserRoles;
+
+public class GetUserRolesHandler : FastEndpoints.ICommandHandler<GetUserRolesCommand, Ardalis.Result.Result<List<string>>>, MediatR.IRequestHandler<GetUserRolesCommand, Ardalis.Result.Result<List<string>>>
+{
+    private readonly IIdentityService identityService;
+
+    public GetUserRolesHandler(IIdentityService identityService)
+    {
+        this.identityService = identityService;
+    }
+
+    public async Task<Ardalis.Result.Result<List<string>>> ExecuteAsync(GetUserRolesCommand request, CancellationToken cancellationToken)
+    {
+        return await Handle(request, cancellationToken);
+    }
+
+    public async Task<Ardalis.Result.Result<List<string>>> Handle(GetUserRolesCommand request, CancellationToken cancellationToken)
+    {
+        var roles = await identityService.GetUserRolesAsync(request.UserId);
+        return Ardalis.Result.Result.Success(roles);
+    }
+
+}
