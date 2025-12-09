5d83acdccba50da2fd1d9a8b50072b7b62c18396
Peter Carroll <petelc1966@hotmail.com>
2025-12-09 18:12:37 -0500

    Removed or Commented out Contributer

--- src/FormDesignerAPI.UseCases/Contributors/Get/GetContributorQuery.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace FormDesignerAPI.UseCases.Contributors.Get;
-
-public record GetContributorQuery(int ContributorId) : IQuery<Result<ContributorDTO>>;
--- tests/FormDesignerAPI.AspireTests/AspireIntegrationTests.cs
+++ tests/FormDesignerAPI.AspireTests/AspireIntegrationTests.cs
@@ -6,38 +6,38 @@ public class AspireIntegrationTests
 {
     // Follow the link below to write you tests with Aspire
     // https://learn.microsoft.com/en-us/dotnet/aspire/testing/write-your-first-test?pivots=xunit
-    [Fact]
-    public async Task GetWebResourceContributersReturnsOkStatusCode()
-    {
-        // Arrange
-        var builder = await DistributedApplicationTestingBuilder
-            .CreateAsync<Projects.FormDesignerAPI_AspireHost>();
+    // [Fact]
+    // public async Task GetWebResourceContributersReturnsOkStatusCode()
+    // {
+    //     // Arrange
+    //     var builder = await DistributedApplicationTestingBuilder
+    //         .CreateAsync<Projects.FormDesignerAPI_AspireHost>();
 
-        builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
-        {
-            clientBuilder.AddStandardResilienceHandler();
-        });
+    //     builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
+    //     {
+    //         clientBuilder.AddStandardResilienceHandler();
+    //     });
 
-        // To capture logs from your tests, see the "Capture logs from tests" section
-        // in the documentation or refer to LoggingTest.cs for a complete example
+    //     // To capture logs from your tests, see the "Capture logs from tests" section
+    //     // in the documentation or refer to LoggingTest.cs for a complete example
 
-        await using var app = await builder.BuildAsync();
+    //     await using var app = await builder.BuildAsync();
 
-        await app.StartAsync();
+    //     await app.StartAsync();
 
-        // Act
-        var httpClient = app.CreateHttpClient("web");
+    //     // Act
+    //     var httpClient = app.CreateHttpClient("web");
 
-        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
-        await app.ResourceNotifications.WaitForResourceHealthyAsync(
-            "web",
-            cts.Token);
+    //     using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+    //     await app.ResourceNotifications.WaitForResourceHealthyAsync(
+    //         "web",
+    //         cts.Token);
 
-        var response = await httpClient.GetAsync("/Contributors", cts.Token);
+    //     var response = await httpClient.GetAsync("/Contributors", cts.Token);
 
-        // Assert
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
+    //     // Assert
+    //     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    // }
 
     [Fact]
     public async Task GetWebResourceFormssReturnsOkStatusCode()
