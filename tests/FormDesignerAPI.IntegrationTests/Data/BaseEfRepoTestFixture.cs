60f72277bb18e7b3ce2828ddbb0d9697b3bd340f
Peter Carroll <petelc1966@hotmail.com>
2025-12-14 09:08:41 -0500

    commented out the integration tests
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- tests/FormDesignerAPI.IntegrationTests/Data/BaseEfRepoTestFixture.cs
+++ tests/FormDesignerAPI.IntegrationTests/Data/BaseEfRepoTestFixture.cs
@@ -1,46 +1,46 @@
-﻿using FormDesignerAPI.Core.FormContext.Aggregates;
-using FormDesignerAPI.Infrastructure.Data;
-using FormDesignerAPI.Infrastructure.Identity;
+﻿// using FormDesignerAPI.Core.FormContext.Aggregates;
+// using FormDesignerAPI.Infrastructure.Data;
+// using FormDesignerAPI.Infrastructure.Identity;
 
-namespace FormDesignerAPI.IntegrationTests.Data;
+// namespace FormDesignerAPI.IntegrationTests.Data;
 
-public abstract class BaseEfRepoTestFixture
-{
-  protected AppDbContext _dbContext;
+// public abstract class BaseEfRepoTestFixture
+// {
+//   protected AppDbContext _dbContext;
 
-  protected BaseEfRepoTestFixture()
-  {
-    var options = CreateNewContextOptions();
-    var _fakeEventDispatcher = Substitute.For<IDomainEventDispatcher>();
+//   protected BaseEfRepoTestFixture()
+//   {
+//     var options = CreateNewContextOptions();
+//     var _fakeEventDispatcher = Substitute.For<IDomainEventDispatcher>();
 
-    _dbContext = new AppDbContext(options, _fakeEventDispatcher);
-  }
+//     _dbContext = new AppDbContext(options, _fakeEventDispatcher);
+//   }
 
-  protected static DbContextOptions<AppDbContext> CreateNewContextOptions()
-  {
-    // Create a fresh service provider, and therefore a fresh
-    // InMemory database instance.
-    var serviceProvider = new ServiceCollection()
-        .AddEntityFrameworkInMemoryDatabase()
-        .BuildServiceProvider();
+//   protected static DbContextOptions<AppDbContext> CreateNewContextOptions()
+//   {
+//     // Create a fresh service provider, and therefore a fresh
+//     // InMemory database instance.
+//     var serviceProvider = new ServiceCollection()
+//         .AddEntityFrameworkInMemoryDatabase()
+//         .BuildServiceProvider();
 
-    // Create a new options instance telling the context to use an
-    // InMemory database and the new service provider.
-    var builder = new DbContextOptionsBuilder<AppDbContext>();
-    builder.UseInMemoryDatabase("cleanarchitecture")
-           .UseInternalServiceProvider(serviceProvider);
+//     // Create a new options instance telling the context to use an
+//     // InMemory database and the new service provider.
+//     var builder = new DbContextOptionsBuilder<AppDbContext>();
+//     builder.UseInMemoryDatabase("cleanarchitecture")
+//            .UseInternalServiceProvider(serviceProvider);
 
-    return builder.Options;
-  }
+//     return builder.Options;
+//   }
 
 
-  protected EfRepository<Form> GetFormRepository()
-  {
-    return new EfRepository<Form>(_dbContext);
-  }
+//   protected EfRepository<Form> GetFormRepository()
+//   {
+//     return new EfRepository<Form>(_dbContext);
+//   }
 
-  // protected EfRepository<ApplicationUser> GetUserRepository()
-  // {
-  //   return new EfRepository<ApplicationUser>(_dbContext);
-  // }
-}
+//   // protected EfRepository<ApplicationUser> GetUserRepository()
+//   // {
+//   //   return new EfRepository<ApplicationUser>(_dbContext);
+//   // }
+// }
